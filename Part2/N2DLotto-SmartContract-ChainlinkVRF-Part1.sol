cd622c956dec5bff7c2a74f657d1cf7faee16043
Net2Dev <info@a3b.io>
2023-03-06 11:06:56 -0800

    Update N2DLotto-SmartContract-ChainlinkVRF-Part1.sol

--- Part2/N2DLotto-SmartContract-ChainlinkVRF-Part1.sol
+++ Part2/N2DLotto-SmartContract-ChainlinkVRF-Part1.sol
@@ -49,6 +49,10 @@ contract N2DLotto is VRFConsumerBaseV2, Ownable {
 
     VRFCoordinatorV2Interface COORDINATOR;
 
+    /* 
+    The values below are for Polygon Mumbai Testnet. Change if needed.
+    */
+    
     uint64 private s_subscriptionId;
     address private vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
     bytes32 private keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
